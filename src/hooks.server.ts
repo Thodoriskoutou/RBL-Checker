58bd1aa012a38df92cecbf869a28cb707f8d54d7
thodoris <thodoriskoutouritos@gmail.com>
2026-01-16 13:57:10 +0200

    intial commit

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,7 @@ import { redirect, type Handle } from '@sveltejs/kit'
 export const handle: Handle = async ({ event, resolve }) => {
 
     if (event.url.pathname === '/') {
-        redirect(303, '/dashboard')
+        redirect(303, '/ips')
     }
 
     const response = await resolve(event)
